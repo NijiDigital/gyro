5e2b8af20937bbc9a5a0a654634d2dddc6ad75b4
Olivier Halligon <olivier@halligon.net>
2017-05-02 22:04:34 +0200

    [Rubocop] Style/DotPosition

--- lib/gyro/utils/string_xcdatamodel.rb
+++ lib/gyro/utils/string_xcdatamodel.rb
@@ -37,11 +37,11 @@ class String
   end
 
   def underscore
-    self.gsub(/::/, '/').
-        gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
-        gsub(/([a-z\d])([A-Z])/, '\1_\2').
-        tr('-', '_').
-        downcase
+    self.gsub(/::/, '/')
+        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+        .tr('-', '_')
+        .downcase
   end
 
   def add_quotes
